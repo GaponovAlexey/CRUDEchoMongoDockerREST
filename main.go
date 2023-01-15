0831b1b8b412a07ffb221ed773cbddb35ea9185f
kekelberi <alexei.gaponov@gmail.com>
2023-01-15 14:12:38 -0600

    save

--- main.go
+++ main.go
@@ -2,47 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 )
 
-type Product struct {
-	ID          primitive.ObjectID `json:"_id" bson:"_id"`
-	Name        string             `json:"product_name" bson:"product_name"`
-	Price       int                `json:"price" bson:"price"`
-	Currency    string             `json:"currency" bson:"currency"`
-	Quantity    string             `json:"quantity" bson:" quantity"`
-	Discount    int                `json:"discount,omitempty" bson:"discount,omitempty"`
-	Vendor      string             `json:"vendor" bson:"vendor"`
-	Accessories []string           `json:"accessories,omitempty" bson:"accessories,omitempty"`
-	SkuID       string             `json:"sku_id" bson:"sku_id"`
+type User struct {
+	FirstName string `bson:"first_name"`
+	LastName  string `bson:"last_name"`
 }
 
-var iphone10 = Product{
-	ID:          primitive.NewObjectID(),
-	Name:        "Iphone15",
-	Price:       900,
-	Currency:    "CAD",
-	Quantity:    "40",
-	Vendor:      "apple",
-	Accessories: []string{"charger", "headset", "slot"},
-	SkuID:       "1234",
-}
-var trimmer = Product{
-	ID:          primitive.NewObjectID(),
-	Name:        "easy trimmer",
-	Price:       100,
-	Currency:    "CAD",
-	Quantity:    "140",
-	Vendor:      "Bosh",
-	Accessories: []string{"charger", "headset", "slot"},
-	SkuID:       "1234",
-}
+var (
+	ctxB = context.WithTimeout(context.)
+)
 
 func cancelError(e error) {
 	if e != nil {
@@ -50,19 +27,24 @@ func cancelError(e error) {
 	}
 }
 
+func insertData(col *mongo.Collection, user User) (*mongo.InsertOneResult, error) {
+	res, err := col.InsertOne(ctxB, user)
+}
+
 func main() {
-	log.Println("start")
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	cancelError(err)
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	cancelError(err)
+	fmt.Println("start")
+	// ctx, cancel := context.WithTimeout(ctxB, 2*time.Second)
+	// defer cancel()
+	client, err := mongo.Connect(ctxB, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer cancelError(err)
+	db := client.Database("tronics").Collection("products")
 
-	db := client.Database("tronics")
-	collection := db.Collection("products")
-	res, err := collection.InsertOne(context.Background(), iphone10)
-	cancelError(err)
-	log.Println(res.InsertedID.(primitive.ObjectID).Timestamp())
+	update := bson.M{"product_name": "Iphone33"}
 
+	ok, err := db.DeleteOne(ctx, update)
+
+	log.Println(ok)
+
+	//end
+	fmt.Println("end")
 }
